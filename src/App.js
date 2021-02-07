7efde886729f8c529b1bd7724e70e9151e0cc06b
Kevin Kim <jhkevin21@gmail.com>
2021-02-07 15:53:22 -0700

    1. Create a function to generate four random number as an answer for    Number baseball 2. Initial states set up for (tries, value, result, answer) 3. Create onSubmit skeleton function 4. Create onChange function to reflect input value on the web page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
 
-function App() {
+const getNumbers = () => {
+  const candidateNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  console.log(`initial candidate numbers: ${candidateNumbers}`);
+  const selectedNumber = [];
+  for (let i = 0; i < 4; i++) {
+    const selectedIndex = Math.floor(
+      Math.random() * (candidateNumbers.length - i)
+    );
+    selectedNumber.push(candidateNumbers.splice(selectedIndex, 1).toString());
+  }
+
+  console.log(selectedNumber);
+  return selectedNumber;
+};
+
+const App = () => {
+  const [tries, setTries] = useState(0);
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const [answer, setAnswer] = useState(getNumbers);
+  const inputRef = useRef(null);
+
+  console.log(`answer: ${answer}`);
+  console.log(typeof answer[0]);
+
+  const onChangeInput = (c) => {
+    setValue(c.target.value);
+  };
+
+  //onChange={onChangeInput}
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(`value: ${value}`);
+
+    inputRef.current.focus();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={onSubmit}>
+        <input
+          ref={inputRef}
+          type='text'
+          value={value}
+          maxLength='4'
+          onChange={onChangeInput}></input>
+      </form>
+      <div>
+        <span>Tries: {tries}</span>
+      </div>
+      <ul>
+        <li>
+          <p>{value}</p>
+          <p>{result}</p>
+        </li>
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
